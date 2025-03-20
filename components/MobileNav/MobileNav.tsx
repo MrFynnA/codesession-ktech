e2306732c0499009e64b7df0c229828e71c988e7
NFA <mac@Macs-MacBook-Pro.local>
2025-03-20 13:24:06 +0000

    update

--- components/MobileNav/MobileNav.tsx
+++ components/MobileNav/MobileNav.tsx
@@ -2,26 +2,33 @@ import React from 'react'
 import Plusicon from '../icons/Plusicon'
 import AccountIcon from '../icons/AccountIcon'
 import AppMenuIcon from '../icons/AppMenuIcon'
+import Link from 'next/link'
 
 const MobileNav = () => {
 
      let mobileMenu:{
         title:string,
-        icon:any
+        icon:any,
+        link:string
      }[]
 
      mobileMenu=[
         {
             title:'History',
-            icon:<AppMenuIcon/>
+            icon:<AppMenuIcon/>,
+            link:''
         },
         {
             title:'Request',
-            icon:<Plusicon/>  
+            icon:<Plusicon/> ,
+            link:''
+
         },
         {
             title:'Settings',
-            icon:<AccountIcon/>  
+            icon:<AccountIcon/> ,
+            link:'/account-settings'
+
         }
      ]
 
@@ -29,10 +36,10 @@ const MobileNav = () => {
   return (
     <div className='w-[90%] flex justify-center items-center bg-white'>
      <ul className='[&>*]:flex [&>*]:flex-col [&>*]:items-center [&>*]:text-sm flex items-center justify-between w-full py-[34px]'>
-       {mobileMenu?.map((item,idx)=><li key={idx}>
+       {mobileMenu?.map((item,idx)=><Link href={item.link} key={idx}>
             <div>{item.icon}</div>
             <p className={`${mobileMenu.length - 1 === idx ? '!text-[#0130C7]' : ''}`}>{item.title}</p>
-        </li>)}
+        </Link>)}
      </ul>
     </div>
   )
